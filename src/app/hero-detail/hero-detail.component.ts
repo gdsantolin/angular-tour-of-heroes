399ef958be5104e1e133a683dbe903086a020a79
Gustavo Dutra <gdsantolin@gmail.com>
2023-11-03 17:57:50 -0300

    add, update, delete heroes features

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -30,6 +30,14 @@ export class HeroDetailComponent {
     this.location.back();
   }
 
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+
+    }
+  }
+
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
